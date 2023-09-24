41b147057ee77988a7179adfb30174e5e7feeabd
DecoDJopa <144096663+DecoDJopa@users.noreply.github.com>
2023-09-25 00:37:24 +0500

    by Jopa
    :)

---
+++ config.py
@@ -0,0 +1,10 @@
+
+admin = [5887700524,2037657499]
+
+bot_token = '5806497144:AAGiwDoysvrzc_9eLy3BWvIMzvYBRK37S6s'
+bottoken = '5806497144:AAGiwDoysvrzc_9eLy3BWvIMzvYBRK37S6s'
+
+bd_host = '91.227.16.16'
+bd_login = 'h159987_posting'
+bd_pass = 'jopa1337123'
+bd_base = 'h159987_bbb1'
